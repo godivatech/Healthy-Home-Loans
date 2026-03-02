c7d4609d8c42dcce940e5a32bcfadfab60ad8a9c
godivatech <infogodivatech@gmail.com>
2026-03-02 18:44:18 +0530

    hero section image change

--- src/sections/Navbar/components/NavbarBrand.tsx
+++ src/sections/Navbar/components/NavbarBrand.tsx
@@ -4,12 +4,12 @@ export const NavbarBrand = () => {
     return (
         <a
             href="/"
-            className="text-black text-xl box-border caret-transparent block leading-[30px] text-nowrap mr-4 pl-3 md:pl-0"
+            className="text-black text-xl box-border caret-transparent flex items-center leading-[30px] text-nowrap mr-4 pl-3 md:pl-0 h-full"
         >
             <img
                 alt="Healthy Home Loans"
                 src={logo}
-                className="text-transparent h-[110px] w-auto box-border max-w-full text-nowrap object-contain scale-[1.8]"
+                className="text-transparent h-[100px] w-auto box-border max-w-full text-nowrap object-contain scale-[2.5]"
             />
         </a>
     );
--- src/sections/Navbar/index.tsx
+++ src/sections/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { NavbarContainer } from "@/sections/Navbar/components/NavbarContainer";
 
 export const Navbar = () => {
     return (
-        <div className="bg-white box-border caret-transparent py-2.5 md:bg-transparent md:py-0">
+        <div className="bg-white box-border caret-transparent py-2 md:bg-white md:py-2 border-b border-gray-200 sticky top-0 z-50">
             <NavbarContainer />
         </div>
     );
